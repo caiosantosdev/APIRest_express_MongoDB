9610d5b95594457c726a79f231ccc908a6ad77ec
Caio Santos <caiosantosdev@gmail.com>
2024-01-10 20:23:38 -0300

    Add routes ReadOne / Update Book

--- src/app.js
+++ src/app.js
@@ -14,6 +14,12 @@ const books = [
     }
 ];
 
+function bookSearch(id){
+    return books.findIndex(currentBook =>{
+        return currentBook.id === Number(id);
+    });
+}
+
 app.get("/", (req,res) => {
     res.status(200).send("Curso de node.js");
 });
@@ -22,10 +28,23 @@ app.get("/books", (req, res) => {
     res.status(200).json(books);
 });
 
+app.get("/books/:id", (req, res) => {
+    const index = bookSearch(req.params.id);
+    res.status(200).json(books[index]);
+});
+
 app.post("/books", (req, res) => {
     const book = req.body;
     books.push(book);
     res.status(201).send("Livro cadastrado com sucesso");
 });
+
+app.put("/books/:id", (req, res) => {
+    const index = bookSearch(req.params.id);
+    books[index].titulo = req.body.titulo;
+    res.status(200).json(books[index]);
+});
+
+
 
 export default app;
